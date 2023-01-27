3dee1e764a4dd0b16d4380b96843fe4ca08e1af0
SalikhovR <107384930+SalikhovR@users.noreply.github.com>
2023-01-27 15:48:16 +0500

    created product factory and migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+//        \App\Models\Product::factory(10)->create();
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
