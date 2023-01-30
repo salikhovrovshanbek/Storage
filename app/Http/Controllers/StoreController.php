c259124305642cf6534622aa903ef99cb494d94b
SalikhovR <107384930+SalikhovR@users.noreply.github.com>
2023-01-30 09:47:44 +0500

    added controllers

---
+++ app/Http/Controllers/StoreController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Store;
+use Illuminate\Http\Request;
+
+class StoreController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Store $store)
+    {
+        //
+    }
+    public function edit(Store $store)
+    {
+        //
+    }
+
+    public function update(Request $request, Store $store)
+    {
+        //
+    }
+
+    public function destroy(Store $store)
+    {
+        //
+    }
+}
